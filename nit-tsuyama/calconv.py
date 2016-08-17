15fc2dcf85ea2a85c0a87d85b045a252f96b16ab
rikyuusima <equanz324@gmail.com>
2016-08-18 03:00:33 +0900

    create 津山高専HPgyoujiVer4用 TODO HTMLスタイルのブレによる問題の修正

--- nit-tsuyama/calconv.py
+++ nit-tsuyama/calconv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#import datetime
 import sys
 import calendar
 import requests
 import zenhan
+import argparse
+import re
 
 class CSV_Struct:
     """.csvに書き込む際の構造体."""
@@ -17,12 +18,12 @@ class CSV_Struct:
         self.ALL_DAY = 'TRUE'
 
 def month_search(line):
-    """月の行かコメントアウト行かを検出し,intで返す."""
+    """月の行かコメントアウト行か予定の行かを検出し,intで返す."""
     # 月の表示のテンプレート
-    month_temp = ['<h3>＜', '月＞</h3>']
+    month_temp = ['<h4>＜', '月＞</h4>']
     # 検索方法が汚すぎる
     if month_temp[0] in line and month_temp[1] in line:
-        month = int(zenhan.h2z(line.replace(month_temp[0], '').replace(month_temp[1], '')))
+        month = int(zenhan.h2z(line.replace(month_temp[0], '').replace(month_temp[1], '').replace(' ', '')))
         return month
     # コメントアウトされた予定を除外
     elif '<!--' in line:
@@ -40,13 +41,13 @@ def month_search(line):
 
 def span_search(line):
     """期間予定であるかを検出し,boolで返す."""
-    zen_tilde = '〜'
-    # '〜'が最初に検出された列
+    zen_tilde = '～'
+    # '～'が最初に検出された列
     index = line.find(zen_tilde)
     # .findで検出されない場合-1が返される
     if index != -1:
-        # 2〜5年といった学年の間隔を〜記号で表しているため、その対策
-        # 〜の２つ後にあるというスタイルに依存
+        # 2～5年といった学年の間隔を～記号で表しているため、その対策
+        # ～の２つ後にあるというスタイルに依存
         # つまり一日予定
         if line.startswith('年', index + 2):
             return -1
@@ -87,7 +88,7 @@ def date_start_search(line):
 
 def date_end_search(line):
     """月表記のない予定終了の日付を検出し,strで返す."""
-    zen_tilde = '〜'
+    zen_tilde = '～'
     # 全角スペース
     zen_space = '　'
     # 全角0
@@ -103,7 +104,7 @@ def date_end_search(line):
 
 def month_date_end_search(line):
     """月表記のある予定終了の日付を検出し,intとintで返す."""
-    zen_tilde = '〜'
+    zen_tilde = '～'
     # 全角スペース
     zen_space = '　'
     # 全角0
@@ -144,12 +145,8 @@ def sub_search(line):
     """予定内容を検出し,strで返す."""
     # 全角スペース
     zen_space = '　'
-    # 改行マークアップ
-    br = '<br>'
     # 右端から検索
     index = line.rfind(zen_space)
-    if line.find(br):
-        line = line.replace(br, '')
 
     # 学校か寮かのタグ付け
     dormitory = '寮'
@@ -159,6 +156,37 @@ def sub_search(line):
         return dormitory_tag + line[index + 1:]
     else:
         return school_tag + line[index + 1:]
+
+def remove_garbage(line):
+    """内容のうち,検出に必要ないものを取り除き,strで返す."""
+    # 曜日
+    days = ['（日）', '（月）', '（火）', '（水）', '（木）', '（金）', '（土）']
+    # 半角space
+    han_space = ' '
+    # 全角space
+    zen_space = '　'
+    # ulタグ
+    ul = ['<ul>', '</ul>']
+    # liタグ
+    li = ['<li>', '</li>']
+
+    # 曜日表記を除去する
+    for day in days:
+        line = line.replace(day, '')
+
+    # ulタグを取り除く
+    line = line.replace(ul[0], '')
+    line = line.replace(ul[1], '')
+
+    # 文字列左の空白列を取り除く
+    line = line.lstrip()
+    # 文字列左に半角スペースを追加(ヤケクソガバガバソース)
+    line = " " + line[0:]
+
+    # liタグを取り除く
+    line = line.replace(li[0], '')
+    line = line.replace(li[1], '')
+    return line
 
 def yesno():
     """yes/no入力の判定を行う."""
@@ -170,199 +198,209 @@ def yesno():
     else:
         raise ValueError("\tA y or n response is required\n\t\t" + s + " is not defined")
 
-# 津山工業高等専門学校 行事予定 URL(2016/04/03現在)
-# まあURL変わるようだったら入力制にするかも
-# (2016/04/03現在)サイトはShift-JIS(半ギレ
-URL = "http://www.tsuyama-ct.ac.jp/honkou/annai/gyouji.htm"
-# カレンダーで読み込み可能にする為の.csv用ヘッダ
-CSV_HEAD = 'Subject,Start Date,End Date,All Day Event\n'
+def main():
+    # 津山工業高等専門学校 行事予定 URL(2016/08/17現在)
+    # まあ毎年URL変わるようだったら入力制にするかも
+    URL = "http://www.tsuyama-ct.ac.jp/gyoujiVer4/gyouji.html"
+    # カレンダーで読み込み可能にする為の.csv用ヘッダ
+    CSV_HEAD = 'Subject,Start Date,End Date,All Day Event\n'
 
-print("4月~3月までの一年間の行事予定を出力します")
-print("URL:" + URL)
-print("予定表に対応する西暦を年度で入力してください")
+    print("4月~3月までの一年間の行事予定を出力します")
+    print("URL:" + URL)
+    print("予定表に対応する西暦を年度で入力してください")
 
-# コマンドライン引数の個数を取得
-argc = len(sys.argv)
+    # コマンドライン引数の個数を取得
+    argc = len(sys.argv)
 
-# ヘルプオプション検出用
-HELP_OPTION = ['-h', '--help']
-# 西暦入力オプション検出用
-YEAR_OPTION = ['-y', '--year']
-# 予定切り分けオプション(ex.oo開始, oo終了)検出用
-LIMIT_OPTION = ['-l', '--limit']
+    # ヘルプオプション検出用
+    HELP_OPTION = ['-h', '--help']
+    # 西暦入力オプション検出用
+    YEAR_OPTION = ['-y', '--year']
+    # 予定切り分けオプション(ex.oo開始, oo終了)検出用
+    LIMIT_OPTION = ['-l', '--limit']
 
-# 西暦の入力
-if argc == 1:
-    year = input()
-    if year.isdigit():
-        year = int(year)
+    # 西暦の入力
+    if argc == 1:
+        year = input()
+        if year.isdigit():
+            year = int(year)
+        else:
+            raise ValueError("\tA Number response is required\n\t\t" + year + " is not defined")
     else:
-        raise ValueError("\tA Number response is required\n\t\t" + year + " is not defined")
-else:
-    year = sys.argv[1]
-    if year.isdigit():
-        year = int(year)
-        print(year)
-    else:
-        raise ValueError("\tA Number response is required\n\t\t" + year + " is not defined")
+        year = sys.argv[1]
+        if year.isdigit():
+            year = int(year)
+            print(year)
+        else:
+            raise ValueError("\tA Number response is required\n\t\t" + year + " is not defined")
 
-if year <= 2000 or year >= 2027:
-    print("注意:このソフトの制作年から大きく異なるようです\n本当にこの西暦でよろしいですか\ny/n")
-    if yesno():
-        pass
-    else:
-        sys.exit()
+    if year <= 2000 or year >= 2027:
+        print("注意:このソフトの制作年から大きく異なるようです\n本当にこの西暦でよろしいですか\ny/n")
+        if yesno():
+            pass
+        else:
+            sys.exit()
 
-# Requests オブジェクト
-r = requests.get(URL)
-# 曜日(除去用)
-days = ['（日）', '（月）', '（火）', '（水）', '（木）', '（金）', '（土）']
-# ISO-8859-1のエンコーディングを変更(半ギレ
-if r.encoding == 'ISO-8859-1':
-    r.encoding = 'Shift_JIS'
+    # Requests オブジェクト
+    r = requests.get(URL)
 
-f = open('./gyouji.html', 'w')
-# r.textの文字コードはutf-8になる
-f.write(r.text)
-r = None
-f.close()
+    # ステータスコードによる例外処理
+    if r.status_code != 200:
+        raise ConnectionError("\tStatus Code is not equal 200.\n\tStatus Code: " + r.status_code)
 
-f = open('./gyouji.html', 'r')
-# ファイルから一行ごとリストに読み出し
-lines = f.readlines()
-f.close()
+    # Encodingをutf-8に変更
+    if r.encoding == 'ISO-8859-1':
+        r.encoding = 'utf-8'
 
-f = open('./schedule.csv', 'w')
-# .csvにヘッダを書き込む
-f.write(CSV_HEAD)
-f.close()
+    # ローカルに書き込み
+    f = open('./gyouji.html', 'w')
+    f.write(r.text)
+    r = None
+    f.close()
 
-# 後ろの改行文字を取り除く
-i = 0
-for line in lines:
-    lines[i] = line.rstrip()
-    i = i + 1
+    f = open('./gyouji.html', 'r')
+    # ファイルから一行ごとリストに読み出し
+    lines = f.readlines()
+    f.close()
 
-# 予定表部分終了部分(テキストそのままのため、何か終了検出方法を考えるべき)
-month_end = '<hr class="s60">'
-# 4月の予定が読み込み開始されたかのフラグ
-month_start_flag = False
-# 予定がコメントアウト中の行にあるかのフラグ
-comout_flag = False
-i = 0
-# for-eachで一行のリストごとに処理
-for line in lines:
-    # 曜日表記を除去する
-    for day in days:
-        line = line.replace(day, '')
+    f = open('./schedule.csv', 'w')
+    # .csvにヘッダを書き込む
+    f.write(CSV_HEAD)
+    f.close()
 
-    # 改行のみの行を取り除く
-    if line != '':
-        month_searching = month_search(line)
-        # 月の行以外はmonthを変えない
-        if month_search(line) >= 1:
-            month = month_searching
-        if month_searching == 4:
-            month_start_flag = True
+    # 後ろの改行文字を取り除く
+    i = 0
+    for line in lines:
+        lines[i] = line.rstrip()
+        i = i + 1
 
-        if month_start_flag:
-            # 予定表部分が終了した場合,breakする
-            if month_end in line:
-                break
-            # コメントアウト行終了時にフラグを下ろす
-            elif month_searching == -1:
-                comout_flag = False
-            # コメントアウト行開始時にフラグを立てる
-            elif month_searching == -2:
-                comout_flag = True
-            # コメントアウト行でないかつ月表示の行でない場合の処理
-            elif not comout_flag and month_searching == 0:
-                # 構造体を渡す
-                csv_write = CSV_Struct()
+    # 予定表部分終了部分(テキストそのままのため、何か終了検出方法を考えるべき)
+    month_end = '</div>'
+    # 4月の予定が読み込み開始されたかのフラグ
+    month_start_flag = False
+    # 予定がコメントアウト中の行にあるかのフラグ
+    comout_flag = False
+    i = 0
+    # for-eachで一行のリストごとに処理
+    for line in lines:
 
-                if month <= 12:
-                    csv_write.start = digit_conv(month) + '/'
+        # 検出に必要のない文字列を取り除く
+        line = remove_garbage(line)
 
-                # 期間予定かの検出
-                span = span_search(line)
-                csv_write.start = csv_write.start + date_start_search(line) + '/'
-                # 一日予定
-                if span == -1:
-                    # 月末の場合は次の月の始めを設定
-                    date_end = date_end_next(year, month, int(date_start_search(line)))
-                    if date_end[0]:
-                        if month == 12:
-                            jan = '01'
-                            csv_write.end = jan + '/'
+        # 改行のみの行を取り除く
+        # remove_garbage()のガバガバ操作の影響でソースが気持ち悪い
+        # 変なHTMLソースに対応するため
+        if line != " ":
+            print(line)
+            month_searching = month_search(line)
+            # 月の行以外はmonthを変えない
+            if month_search(line) >= 1:
+                month = month_searching
+            if month_searching == 4:
+                month_start_flag = True
+
+            if month_start_flag:
+                # 予定表部分が終了した場合,breakする
+                if month_end in line:
+                    break
+                # コメントアウト行終了時にフラグを下ろす
+                elif month_searching == -1:
+                    comout_flag = False
+                # コメントアウト行開始時にフラグを立てる
+                elif month_searching == -2:
+                    comout_flag = True
+                # コメントアウト行でないかつ月表示の行でない場合の処理
+                elif not comout_flag and month_searching == 0:
+                    # 構造体を渡す
+                    csv_write = CSV_Struct()
+
+                    if month <= 12:
+                        csv_write.start = digit_conv(month) + '/'
+
+                    # 期間予定かの検出
+                    span = span_search(line)
+                    csv_write.start = csv_write.start + date_start_search(line) + '/'
+                    # 一日予定
+                    if span == -1:
+                        # 月末の場合は次の月の始めを設定
+                        date_end = date_end_next(year, month, int(date_start_search(line)))
+                        if date_end[0]:
+                            if month == 12:
+                                jan = '01'
+                                csv_write.end = jan + '/'
+                            else:
+                                csv_write.end = digit_conv(month + 1) + '/'
                         else:
-                            csv_write.end = digit_conv(month + 1) + '/'
-                    else:
-                        csv_write.end = digit_conv(month) + '/'
+                            csv_write.end = digit_conv(month) + '/'
 
-                    csv_write.end = csv_write.end + date_end[1] + '/'
-                # 期間予定
-                elif span == 0:
-                    date_end = date_end_next(year, month, int(date_end_search(line)))
-                    if date_end[0]:
-                        if month == 12:
-                            jan = '01'
-                            csv_write.end = jan + '/'
+                        csv_write.end = csv_write.end + date_end[1] + '/'
+                    # 期間予定
+                    elif span == 0:
+                        date_end = date_end_next(year, month, int(date_end_search(line)))
+                        if date_end[0]:
+                            if month == 12:
+                                jan = '01'
+                                csv_write.end = jan + '/'
+                            else:
+                                csv_write.end = digit_conv(month + 1) + '/'
                         else:
-                            csv_write.end = digit_conv(month + 1) + '/'
-                    else:
-                        csv_write.end = digit_conv(month) + '/'
+                            csv_write.end = digit_conv(month) + '/'
 
-                    csv_write.end = csv_write.end + date_end[1] + '/'
-                # 月をまたぐ期間予定
-                elif span == -2:
-                    mde = month_date_end_search(line)
-                    date_end = date_end_next(year, int(mde[0]), int(mde[1]))
-                    if date_end[0]:
-                        if month == 12:
-                            jan = '01'
-                            csv_write.end = jan + '/'
+                        csv_write.end = csv_write.end + date_end[1] + '/'
+                    # 月をまたぐ期間予定
+                    elif span == -2:
+                        mde = month_date_end_search(line)
+                        date_end = date_end_next(year, int(mde[0]), int(mde[1]))
+                        if date_end[0]:
+                            if month == 12:
+                                jan = '01'
+                                csv_write.end = jan + '/'
+                            else:
+                                csv_write.end = digit_conv(int(mde[0]) + 1) + '/'
                         else:
-                            csv_write.end = digit_conv(int(mde[0]) + 1) + '/'
+                            csv_write.end = digit_conv(int(mde[0])) + '/'
+
+                        csv_write.end = csv_write.end + date_end[1] + '/'
+
+                    # 1日
+                    first = '01'
+                    # startは1~3月なら次の年を設定
+                    if month < 4:
+                        csv_write.start = csv_write.start + str(year + 1)
                     else:
-                        csv_write.end = digit_conv(int(mde[0])) + '/'
+                        csv_write.start = csv_write.start + str(year)
+                    decem = 12
+                    # endは12月予定かつ月を跨いでいるまたは,1~3月なら次の年を設定
+                    if month == decem and span == -2 or month == decem and date_end[0] or month < 4:
+                        csv_write.end = csv_write.end + str(year + 1)
+                    else:
+                        csv_write.end = csv_write.end + str(year)
+                    csv_write.sub = sub_search(line)
 
-                    csv_write.end = csv_write.end + date_end[1] + '/'
+                    # 各種表示
+                    print("start: " + csv_write.start)
+                    print("end: " + csv_write.end)
+                    print("sub: " + csv_write.sub)
 
-                # 1日
-                first = '01'
-                # startは1~3月なら次の年を設定
-                if month < 4:
-                    csv_write.start = csv_write.start + str(year + 1)
-                else:
-                    csv_write.start = csv_write.start + str(year)
-                decem = 12
-                # endは12月予定かつ月を跨いでいるまたは,1~3月なら次の年を設定
-                if month == decem and span == -2 or month == decem and date_end[0] or month < 4:
-                    csv_write.end = csv_write.end + str(year + 1)
-                else:
-                    csv_write.end = csv_write.end + str(year)
-                csv_write.sub = sub_search(line)
+                    # .csvに書き込む
+                    f = open('./schedule.csv', 'a')
+                    f.write(csv_write.sub + "," +
+                            csv_write.start + "," +
+                            csv_write.end + "," +
+                            csv_write.ALL_DAY + "\n")
+                    f.close()
 
-                # 各種表示
-                print("start: " + csv_write.start)
-                print("end: " + csv_write.end)
-                print("sub: " + csv_write.sub)
+    # ダウンロードしたWebページの削除
+    f = open('./gyouji.html', 'w')
+    f.write('')
+    f.close()
 
-                # .csvに書き込む
-                f = open('./schedule.csv', 'a')
-                f.write(csv_write.sub + "," +
-                        csv_write.start + "," +
-                        csv_write.end + "," +
-                        csv_write.ALL_DAY + "\n")
-                f.close()
+    print("\nSuccessful!")
 
-# ダウンロードしたWebページの削除
-f = open('./gyouji.html', 'w')
-f.write('')
-f.close()
-
-print("\nSuccessful!")
+# モジュール利用された場合のため
+if __name__ == '__main__':
+    main()
 
 # Issue: Webサイトソースが改行(CR, CRLF, LF)を用いず<br>として形式を保っている場合に対応できていない.
-#        期間制予定かの検出に〜の２つ後にあるというスタイルに依存した判定を行っている.
+#        期間制予定かの検出に～の２つ後にあるというスタイルに依存した判定を行っている.
 #        予定表部分終了部分がテキストそのまま.
